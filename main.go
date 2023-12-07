c7c82dd6ff3e7cba7cdbd4e506c6e8b3142ab21d
Evandro Monteiro Lima <goodvandro@gmail.com>
2023-12-07 15:09:19 +0000

    starting using concurrency

--- main.go
+++ main.go
@@ -1,37 +1,19 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
-
-	"github.com/goodvandro/go-intensivo/internal/infra/database"
-	usecase "github.com/goodvandro/go-intensivo/internal/useCase"
-	_ "github.com/mattn/go-sqlite3" // the underline is because the package is not used directly but when it is necessary
+	"time"
 )
 
+func processando() {
+	for i := 0; i < 10; i++ {
+		fmt.Println(i)
+		time.Sleep(time.Second)
+	}
+}
+
 func main() {
-	db, err := sql.Open("sqlite3", "db.sqlite3")
-
-	if err != nil {
-		panic(err)
-	}
-
-	defer db.Close() // wait all processes to execute than close the database
-
-	orderRepository := database.NewOrderRepository(db)
-
-	uc := usecase.NewCalculateFinalPrice(orderRepository)
-
-	input := usecase.OrderInput{
-		ID:    "1234",
-		Price: 10.0,
-		Tax:   1.0,
-	}
-
-	output, err := uc.Execute(input)
-
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println(output)
+	go processando()
+	go processando()
+	processando()
 }
